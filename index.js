ffd87e9b644c0479d1d2d6f548fb529ea3795946
Mel <chanatan@outlook.com>
2022-01-02 00:15:10 +0700

    First commit

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS",
     ]
 })
 
+let pinnedChannelId;
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
 client.on("messageCreate", (message) => {
-    if (message.content == "hi") {
-        message.reply("Hello world.")
+    if (message.content == "!pin") {
+        pinnedChannelId = message.channelId
     }
+})
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(pinnedChannelId).send({
+        content: `<@${member.id}> is now a Communist.`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
