70748370c337ea5e5e167fa493ea7b8cd5daf22f
Mel <chanatan@outlook.com>
2022-01-02 13:48:03 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Discord = require("discord.js")
 require("dotenv").config()
-
 const generateImage = require("./generateImage")
+const generateWelcomeMsg = require("./generateWelcomeMsg")
+
+const PREFIX = "!"
+const TOKEN = process.env.TOKEN
+
+const botJoinedImage = "https://i.imgur.com/VvgqEgw.jpg"
 
 const client = new Discord.Client({
     intents: [
@@ -16,18 +21,57 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("messageCreate", (message) => {
-    if (message.content == "!pin") {
+client.on("guildCreate", async(guild) => {
+    // Set the pinned channel to the first text channel 
+    const firstChannel = guild.channels.cache.find(
+      (c) => c.type === "GUILD_TEXT" && c.permissionsFor(guild.me).has("SEND_MESSAGES")
+    )
+    pinnedChannelId = firstChannel.id
+
+    // Bot joined message 
+    firstChannel.send({
+        content: "All salute, I'm here to bless everyone with the Communist wind of peace💣",
+        files: [botJoinedImage]
+    })
+})
+
+client.on("messageCreate", async(message) => {
+    if (!message.content.startsWith(PREFIX)) return
+
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLocaleLowerCase();
+
+    // Set new Pinned channel
+    if (command === "pin") {
+        if (pinnedChannelId == message.channelId) return message.reply("I'm already Broadcasting here, are you even paying attention 😒")
+
         pinnedChannelId = message.channelId
+        message.reply("I will now Broadcast in this channel. Be proud👍")
     }
+
+    // Poll command
+    if (command === "poll") {
+        let msg = await message.reply("‼We ARE NOT Democratic, but I will let this one pass‼\n" + args.join(" "))
+        await msg.react("✅")
+        await msg.react("❌")
+    }
+
+    // Random command all params are seperated by "or"
+    if(command === "rand") {
+        const choices = args.join(" ").split("or").map( c => c.trim())
+        const randomNum = Math.floor(Math.random() * choices.length);      
+        message.reply(`The Supreme Ruler had decided: ${choices[randomNum]} it is!`)
+    }
+
 })
 
 client.on("guildMemberAdd", async (member) => {
     const img = await generateImage(member)
+    const message = generateWelcomeMsg(member.id)
     member.guild.channels.cache.get(pinnedChannelId).send({
-        content: `<@${member.id}> is now a Communist.`,
+        content: message,
         files: [img]
     })
 })
 
-client.login(process.env.TOKEN)
+client.login(TOKEN)
