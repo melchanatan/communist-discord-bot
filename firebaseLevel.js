1fd0800624b8fb4cd1587f05fae4d2b4778060bc
SecretKr <kraiwit.wit@gmail.com>
2022-01-02 22:36:28 +0700

    server level

--- firebaseLevel.js
+++ firebaseLevel.js
@@ -1,13 +1,13 @@
 const setLevel = (ref, message, level) => {
-  ref.child(String(message.author.id)).set({
+  ref.child(String(message.guild.id)).child(String(message.author.id)).set({
     name: String(message.author.username),
     level: level
   })
 }
 
 const getLevel = async (ref, message) => {
-  ref.child(String(message.author.id)).child("level").once("value", function(snapshot) {
-    message.reply(String(snapshot.val()))
+  ref.child(String(message.guild.id)).child(String(message.author.id)).child("level").once("value", function(snapshot) {
+    if (snapshot.val() != null) message.reply(String(snapshot.val()))
   })
 }
 
